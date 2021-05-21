657fcf417e0e4a7676dd276fdc87f36a9637b105
ManishKarki1997 <manishkarki247@gmail.com>
2021-05-21 19:06:40 +0545

    minor-fix - change API_URL to correct endpoint

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 import {DarkTheme, LightTheme} from './Theme';
-// const API_URL = 'https://genshin-app-api.herokuapp.com';
-const API_URL = 'http://3269957484b2.ngrok.io';
+const API_URL = 'https://genshin-app-api.herokuapp.com';
+// const API_URL = 'http://3269957484b2.ngrok.io';
 
 export {LightTheme, DarkTheme, API_URL};
